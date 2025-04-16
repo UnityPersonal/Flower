0c11ea4765f99164ef33e3dcaf1c22e5932a53f9
seungDev <55948026+agagtmdtlr@users.noreply.github.com>
2025-04-16 17:09:32 +0900

    각 터레인 메쉬 자동 생성
    start 이벤트에서 동적으로 생성하여 할당. 속도 겁나 느림...

--- Assets/01.InGame/GrassTest/TerrainMeshGenerator.cs
+++ Assets/01.InGame/GrassTest/TerrainMeshGenerator.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TerrainMeshGenerator : MonoBehaviour
 {
+    [SerializeField] private Texture2D heightMap;
+    [SerializeField] private Material grassMaterial;
+    [SerializeField] private Material groundMaterial;
     private void Start()
     {
         CreateTerrainMesh();
@@ -13,28 +17,53 @@ public class TerrainMeshGenerator : MonoBehaviour
     void CreateTerrainMesh()
     {
         var meshFilter = GetComponent<MeshFilter>();
-        var mesh = meshFilter.sharedMesh;
+        var originalMesh = meshFilter.sharedMesh;
 
-        // get vertices
-        // 1. Get the vertices of the mesh
-        List<Vector3> vertices = new List<Vector3>();
-        mesh.GetVertices(vertices);
-        
-        // 2. Get the UVs of the mesh
-        List<Vector2> uvs = new List<Vector2>();
-        mesh.GetUVs(0, uvs);
-        
-        for (int i = 0 ; i < vertices.Count ; i++)
+        // 새로운 Mesh 생성
+        Mesh newMesh = new Mesh();
+
+        // 기존 Mesh의 데이터 복사
+        newMesh.vertices = originalMesh.vertices;
+        newMesh.triangles = originalMesh.triangles;
+        newMesh.uv = originalMesh.uv;
+        newMesh.normals = originalMesh.normals;
+        newMesh.tangents = originalMesh.tangents;
+        newMesh.colors = originalMesh.colors;
+
+        // SubMesh 데이터 복사
+        newMesh.subMeshCount = originalMesh.subMeshCount;
+        for (int i = 0; i < originalMesh.subMeshCount; i++)
         {
-            Vector3 v = vertices[i];
-            var worldPos =  transform.TransformPoint(v);
-            v.y += worldPos.magnitude / 100f;
-            vertices[i] = v;
+            newMesh.SetIndices(originalMesh.GetIndices(i), originalMesh.GetTopology(i), i);
         }
         
-        mesh.SetVertices(vertices);
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-        mesh.RecalculateTangents();
+        List<Vector3> vertices = new List<Vector3>();
+        originalMesh.GetVertices(vertices);
+
+        var pixels = heightMap.GetPixels();
+
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            var wp =  transform.TransformPoint(vertices[i]);
+            Vector2 uv = new Vector2(wp.x / heightMap.width, wp.z / heightMap.height);
+            uv *= 2f;
+            
+            Color pixel = heightMap.GetPixelBilinear(uv.x , uv.y);
+
+            var vector3 = vertices[i];
+            vector3.y += pixel.a * 60f; // 높이 조정
+            vertices[i] = vector3;
+        }
+        
+        newMesh.SetVertices(vertices);
+        
+        // Bounds 및 기타 데이터 갱신
+        newMesh.RecalculateBounds();
+        newMesh.RecalculateNormals();
+        newMesh.RecalculateTangents();
+
+        // 새로운 Mesh를 MeshFilter에 설정
+        meshFilter.mesh = newMesh;
+        
     }
 }
